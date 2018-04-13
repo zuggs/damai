1ba647c970b8574af41f2045988a822a80a5b5a6
tailinl82 <lutailin82@163.com>
2018-04-13 17:05:39 +0800

    ltlnew

---
+++ src/components/Live/LiveItemSmall.vue
@@ -0,0 +1,94 @@
+<template>
+<div>
+  <div class="small" v-for="(item,i) in liveData" :key='i'>
+    <div class="small-t">
+      <img width="100%" :src="liveData[i].project_pic" alt="">
+      <p>{{liveData[i].project_name}}</p>
+    </div>
+    <div class="small-b">
+      <h4>{{liveData[i].tips}}</h4>
+      <span>{{liveData[i].sell_tag}}</span>
+    </div>
+    
+</div>
+<div @click="num++" class="huanyihuan">换一换
+  <i class="iconfont icon-huanyihuan"></i>
+  </div>
+</div>
+</template>
+<script>
+import data from '../../../../static/data.json'
+export default {
+  name: 'ItemSmall',
+  data() {
+    return {
+        liveData: (JSON.parse(data.data.result).category_module[0].items).slice(0,6),
+        liveData2: (JSON.parse(data.data.result).category_module[0].items).slice(6,12),
+        liveData3: (JSON.parse(data.data.result).category_module[0].items).slice(12,18),
+        liveData4: (JSON.parse(data.data.result).category_module[0].items).slice(18,24),
+        //arr:[liveData1,liveData2,liveData3,liveData4],
+        num: 0
+    }
+  },
+  created(){
+      // this.arr = [this.liveData1,this.liveData2,this.liveData3,this.liveData4]
+      console.log(this.liveData)
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .small{
+    width: 1.07rem;
+    height: 2.05rem;
+    margin: 0 10px 15px 0;
+    float: left;
+    .small-t{
+      position: relative;
+      img{
+        display: block;
+        margin-bottom: 5px;
+        border-radius: 3px;
+      }
+      p{
+        position: absolute;
+        top: 1.25rem;
+        left: 9px;
+        right: 0;
+        width: .92rem;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-size: 14px;
+        color: #fff;
+        text-align: center;
+      }
+    }
+    .small-b{
+      h4{
+        height: .3rem;
+        line-height: 15px;
+        margin-bottom: 6px;
+        overflow: hidden;
+        -webkit-line-clamp:2;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+        text-overflow: ellipsis;
+        font-size: 10px;
+        color: #666;
+      }
+      span{
+        padding: 0 2px;
+        border: 1px solid #ff1268;
+        font-size: 10px;
+        color: #ff1268;
+        border-radius: 3px;
+      }
+    }
+  }
+  .huanyihuan{
+    font-size: 14px;
+    color: #ff1268;
+    text-align: center;
+    padding-bottom: 6px;
+            }
+</style>
---
+++ src/components/Live/LiveSmallBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <item-small class="small-box"></item-small>
+</template>
+<script>
+import ItemSmall from '../Live/LiveItemSmall'
+export default {
+  name: 'SmallBox',
+  components: {
+      ItemSmall
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .small-box{
+        height: 4.4rem;
+    }
+</style>
